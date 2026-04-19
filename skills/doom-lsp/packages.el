f3d4d4d250a51933ca9f11993c0592dc0cacccee
mutouyuguo <yaq-1944@hotmail.com>
2026-04-19 22:39:27 +0800

    rm skills

--- skills/doom-lsp/packages.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; packages.el --- doom-lsp module packages
-
-;; No additional packages needed - Doom's :tools lsp already provides lsp-mode, lsp-ui, etc.
-;; This file exists to make it a proper Doom module.
-
-(provide 'packages)
-;;; packages.el ends here
